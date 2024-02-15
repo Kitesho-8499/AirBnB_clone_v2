d33e0f2a0aac54610f19de0ead4a6e9080c66be8
Kitesho-8499 <kitesho.kitesh@gmail.com>
2024-02-15 10:02:02 +0000

    All tasks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+""" 3. dd third view func that redirects and has default val for variable """
+
 from flask import Flask
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    '''The home page.'''
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hbnb():
-    '''The hbnb page.'''
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_page(text):
-    '''The c page.'''
-    return 'C {}'.format(text.replace('_', ' '))
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
+@app.route('/python/')
 @app.route('/python/<text>')
-@app.route('/python', defaults={'text': 'is cool'})
-def python_page(text):
-    '''The python page.'''
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,44 +1,46 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+""" 4. Add fourth view function that displays var only if is integer """
+
 from flask import Flask
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    '''The home page.'''
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hbnb():
-    '''The hbnb page.'''
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_page(text):
-    '''The c page.'''
-    return 'C {}'.format(text.replace('_', ' '))
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
+@app.route('/python/')
 @app.route('/python/<text>')
-@app.route('/python', defaults={'text': 'is cool'})
-def python_page(text):
-    '''The python page.'''
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
 @app.route('/number/<int:n>')
-def number_page(n):
-    '''The number page.'''
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
     return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
