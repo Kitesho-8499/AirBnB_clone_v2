59950faf50d8da2480e84a6bfa338988a4d36a04
Eman288 <emantamam107@gmail.com>
2024-02-23 10:14:43 +0000

    0x04-task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-""" 0. Script to start a Flask web application """
-
-from flask import Flask
-
+import flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+def homePage():
+	"""a function to display hello HBNB at the route '/'"""
+	return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main':
+	app.run(debug=True, host='0.0.0.0', port='5000')
